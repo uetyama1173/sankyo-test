feb915ab81974fbb519759401729aca186611e40
uetyama1173 <swow_0310k@yahoo.co.jp>
2023-02-05 22:00:52 +0900

    liffid change

--- repair/liff.js
+++ repair/liff.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     // liffId: LIFF URL "https://liff.line.me/xxx"のxxxに該当する箇所
     // LINE DevelopersのLIFF画面より確認可能
-    var liffId = "1657691237-9Ebwe2mK";
+    var liffId = "1657879176-OvPwBB4r";
     initializeLiff(liffId);
 })
 
